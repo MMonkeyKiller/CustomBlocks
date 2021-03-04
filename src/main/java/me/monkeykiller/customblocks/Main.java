618f83f57c7b5d10ef6a99593883b895dd11b96d
MonkeyKiller <elacertijoyt@gmail.com>
2021-03-04 02:33:50 -0300

    Registering CustomBlocks  - cloth_(0-15)  - rock  - mossy_rock  - cut_copper  - exposed_cut_copper

--- src/main/java/me/monkeykiller/customblocks/Main.java
+++ src/main/java/me/monkeykiller/customblocks/Main.java
@@ -1,6 +1,8 @@
 package me.monkeykiller.customblocks;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Instrument;
+import org.bukkit.Note;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,6 +16,15 @@ public final class Main extends JavaPlugin {
         getLogger().info("Plugin enabled (v." + pdfFile.getVersion() + ")");
         PluginManager pm = Bukkit.getPluginManager();
         pm.registerEvents(new Events(this), this);
+
+        for (int i = 1; i <= 16; i++)
+            new CustomBlock("survitroll:cloth_" + i, Instrument.BANJO, new Note(i), false);
+
+        new CustomBlock("survitroll:rock", Instrument.BANJO, new Note(17), false);
+        new CustomBlock("survitroll:mossy_rock", Instrument.BANJO, new Note(18), false);
+
+        new CustomBlock("survitroll:cut_copper", Instrument.BASS_DRUM, new Note(1), false);
+        new CustomBlock("survitroll:exposed_cut_copper", Instrument.BASS_DRUM, new Note(3), false);
     }
 
     @Override
